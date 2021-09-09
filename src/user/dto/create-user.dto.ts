97f4d497e6c49846af056808eb523ed625f6e722
Kerosz <andrei_chirila@outlook.com>
2021-09-09 23:20:32 +0300

    chore: improved swagger api docs

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -2,18 +2,22 @@ import { ApiProperty } from "@nestjs/swagger";
 import { IsDefined, IsEmail, MaxLength, MinLength } from "class-validator";
 
 export class CreateUserDto {
-  @ApiProperty()
+  @ApiProperty({
+    type: "String",
+    description: "Email address",
+    default: "test@email.com",
+  })
   @IsEmail({}, { message: "Email address must be a valid email!" })
   @IsDefined({ message: "Email address must not be empty!" })
   email!: string;
 
-  @ApiProperty()
+  @ApiProperty({ type: "String", description: "User password", default: "password123" })
   @IsDefined({ message: "Password must not be empty!" })
   @MinLength(6, { message: "Passowrd must have at least 6 characters!" })
   @MaxLength(32, { message: "Password must be at most 32 characters!" })
   password!: string;
 
-  @ApiProperty()
+  @ApiProperty({ type: "String", description: "User full name", default: "Api Fullname" })
   @IsDefined({ message: "Name must not be empty!" })
   @MinLength(4, { message: "Name must be at least 4 characters!" })
   @MaxLength(26, { message: "Name must be at most 26 characters!" })
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,40 +4,62 @@ import {
   ApiBody,
   ApiConflictResponse,
   ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiParam,
+  ApiTags,
 } from "@nestjs/swagger";
 
 import { UserService } from "./user.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
 
+@ApiTags("users")
 @Controller({ version: "1", path: "users" })
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
   @ApiBody({ type: CreateUserDto })
   @ApiCreatedResponse({ description: "The record has been successfully created." })
   @ApiBadRequestResponse({ description: "The record has failed validation." })
-  @ApiConflictResponse({ description: "The record an internal conflict." })
+  @ApiConflictResponse({ description: "The record has an internal conflict." })
+  @ApiForbiddenResponse({ description: "Forbidden!" })
+  @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
+  @ApiOkResponse({ description: "The record has been successfully returned." })
+  @ApiNotFoundResponse({ description: "The record was not found." })
+  @ApiForbiddenResponse({ description: "Forbidden!" })
   @Get()
   index() {
     return this.userService.index();
   }
 
+  @ApiOkResponse({ description: "The record has been successfully returned." })
+  @ApiNotFoundResponse({ description: "The record was not found." })
+  @ApiForbiddenResponse({ description: "Forbidden!" })
+  @ApiParam({ name: "id", type: "String", description: "User id" })
   @Get(":id")
   find(@Param("id") id: string) {
     return this.userService.show(id);
   }
 
+  @ApiOkResponse({ description: "The record has been successfully returned." })
+  @ApiNotFoundResponse({ description: "The record was not found." })
+  @ApiForbiddenResponse({ description: "Forbidden!" })
+  @ApiParam({ name: "id", type: "String", description: "User id" })
   @Patch(":id")
   update(@Param("id") id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(id, updateUserDto);
   }
 
+  @ApiOkResponse({ description: "The record has been successfully returned." })
+  @ApiNotFoundResponse({ description: "The record was not found." })
+  @ApiForbiddenResponse({ description: "Forbidden!" })
+  @ApiParam({ name: "id", type: "String", description: "User id" })
   @Delete(":id")
   remove(@Param("id") id: string) {
     return this.userService.remove(id);
