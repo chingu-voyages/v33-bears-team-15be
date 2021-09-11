6cd4842a0ed9d083833515e077b6cbd30fb38a7d
Kerosz <andrei_chirila@outlook.com>
2021-09-12 00:20:44 +0300

    chore: removed placeholder tests

--- src/user/__mocks__/user.service.ts
+++
@@ -1,9 +0,0 @@
-import { userFixture } from "../test/user.fixture";
-
-export const UserService = jest.fn().mockReturnValue({
-  create: jest.fn().mockResolvedValue(userFixture),
-  index: jest.fn().mockResolvedValue([userFixture]),
-  show: jest.fn().mockResolvedValue(userFixture),
-  update: jest.fn().mockResolvedValue(userFixture),
-  remove: jest.fn().mockResolvedValue(true),
-});
