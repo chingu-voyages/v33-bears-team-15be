49c1dbb551bbf1f9de066d5335b62524f0a72518
Kerosz <andrei_chirila@outlook.com>
2021-09-23 22:41:49 +0300

    chore: add initial api docs for book controller

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,32 +1,65 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from "@nestjs/common";
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiConflictResponse,
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiParam,
+  ApiTags,
+} from "@nestjs/swagger";
+
 import { BookService } from "./book.service";
 import { CreateBookDto } from "./dto/create-book.dto";
 import { UpdateBookDto } from "./dto/update-book.dto";
 
-@Controller("books")
+@ApiTags("books")
+@Controller({ version: "1", path: "books" })
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
+  @ApiBody({ type: CreateBookDto })
+  @ApiCreatedResponse({ description: "The record has been successfully created." })
+  @ApiBadRequestResponse({ description: "The record has failed validation." })
+  @ApiConflictResponse({ description: "The record has an internal conflict." })
+  @ApiForbiddenResponse({ description: "Forbidden!" })
   @Post()
   create(@Body() createBookDto: CreateBookDto) {
     return this.bookService.create(createBookDto);
   }
 
+  @ApiOkResponse({ description: "The record has been successfully returned." })
+  @ApiNotFoundResponse({ description: "The record was not found." })
+  @ApiForbiddenResponse({ description: "Forbidden!" })
   @Get()
-  findAll() {
+  index() {
     return this.bookService.findAll();
   }
 
+  @ApiOkResponse({ description: "The record has been successfully returned." })
+  @ApiNotFoundResponse({ description: "The record was not found." })
+  @ApiForbiddenResponse({ description: "Forbidden!" })
+  @ApiParam({ name: "id", type: "String", description: "Book id" })
   @Get(":id")
-  findOne(@Param("id") id: string) {
+  find(@Param("id") id: string) {
     return this.bookService.findOne(+id);
   }
 
+  @ApiOkResponse({ description: "The record has been successfully returned." })
+  @ApiNotFoundResponse({ description: "The record was not found." })
+  @ApiForbiddenResponse({ description: "Forbidden!" })
+  @ApiParam({ name: "id", type: "String", description: "Book id" })
   @Patch(":id")
   update(@Param("id") id: string, @Body() updateBookDto: UpdateBookDto) {
     return this.bookService.update(+id, updateBookDto);
   }
 
+  @ApiOkResponse({ description: "The record has been successfully returned." })
+  @ApiNotFoundResponse({ description: "The record was not found." })
+  @ApiForbiddenResponse({ description: "Forbidden!" })
+  @ApiParam({ name: "id", type: "String", description: "Book id" })
   @Delete(":id")
   remove(@Param("id") id: string) {
     return this.bookService.remove(+id);
