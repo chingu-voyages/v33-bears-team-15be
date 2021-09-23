252d3b1ab8154777ba99cd3faa98f2baa7c2b721
Kerosz <andrei_chirila@outlook.com>
2021-09-23 15:04:49 +0300

    chore: add initial book crud resource

---
+++ src/book/book.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, Logger } from "@nestjs/common";
+import { CreateBookDto } from "./dto/create-book.dto";
+import { UpdateBookDto } from "./dto/update-book.dto";
+
+@Injectable()
+export class BookService {
+  create(createBookDto: CreateBookDto) {
+    Logger.log(createBookDto);
+    return "This action adds a new book";
+  }
+
+  findAll() {
+    return `This action returns all book`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} book`;
+  }
+
+  update(id: number, updateBookDto: UpdateBookDto) {
+    Logger.log(updateBookDto);
+    return `This action updates a #${id} book`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} book`;
+  }
+}
