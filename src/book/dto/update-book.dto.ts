1dece06b2c2b251ac86059d8586c934d6b2597cb
Kerosz <andrei_chirila@outlook.com>
2021-09-23 22:42:14 +0300

    chore: add DTOs for book

--- src/book/dto/update-book.dto.ts
+++ src/book/dto/update-book.dto.ts
@@ -1,4 +1,30 @@
-import { PartialType } from "@nestjs/swagger";
+import { ApiProperty, PartialType } from "@nestjs/swagger";
+import { IsDate, IsOptional, IsPositive, IsString } from "class-validator";
 import { CreateBookDto } from "./create-book.dto";
 
-export class UpdateBookDto extends PartialType(CreateBookDto) {}
+export class UpdateBookDto extends PartialType(CreateBookDto) {
+  @ApiProperty({ description: "Book file source path", required: false })
+  @IsString({ message: "Path must be of type string!" })
+  @IsOptional()
+  srcPath?: string;
+
+  @ApiProperty({ description: "Book cover image source path", required: false })
+  @IsString({ message: "Path must be of type string!" })
+  @IsOptional()
+  srcCoverPath?: string;
+
+  @ApiProperty({ description: "Book file source path", required: false })
+  @IsDate({ message: "Published Date must be a valid Date type!" })
+  @IsOptional()
+  publishedDate?: Date;
+
+  @ApiProperty({ description: "A list of book review ID references", required: false })
+  @IsString({ each: true, message: "Review Must be of type string!" })
+  @IsOptional()
+  reviews?: string[];
+
+  @ApiProperty({ description: "Total number of book reviews", required: false })
+  @IsPositive({ message: "Total reviews must be a positive number!" })
+  @IsOptional()
+  totalReviews?: number;
+}
