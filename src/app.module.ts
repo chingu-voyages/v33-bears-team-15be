aef24e2fdbc2c71cfa7489a2a11edae5ad32a567
Kerosz <andrei_chirila@outlook.com>
2021-09-25 20:15:05 +0300

    feat: add initial category files

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,17 @@ import { DatabaseModule } from "./db/db.module";
 import { UserModule } from "./user/user.module";
 import { AuthModule } from "./auth/auth.module";
 import { BookModule } from "./book/book.module";
+import { CategoryModule } from "./category/category.module";
 
 @Module({
-  imports: [ConfigModule, DatabaseModule, UserModule, AuthModule, BookModule],
+  imports: [
+    ConfigModule,
+    DatabaseModule,
+    UserModule,
+    AuthModule,
+    BookModule,
+    CategoryModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/category/category.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, Logger } from "@nestjs/common";
+import { CreateCategoryDto } from "./dto/create-category.dto";
+import { UpdateCategoryDto } from "./dto/update-category.dto";
+
+@Injectable()
+export class CategoryService {
+  create(createCategoryDto: CreateCategoryDto) {
+    Logger.log(createCategoryDto);
+    return "This action adds a new category";
+  }
+
+  findAll() {
+    return `This action returns all category`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} category`;
+  }
+
+  update(id: number, updateCategoryDto: UpdateCategoryDto) {
+    Logger.log(updateCategoryDto);
+    return `This action updates a #${id} category`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} category`;
+  }
+}
