615c25da4bacd13d8ed986180a7e012b999cc7d9
Kerosz <andrei_chirila@outlook.com>
2021-09-25 04:32:38 +0300

    chore: update book controller & improved endpoint decorators

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,4 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  UploadedFiles,
+  UseGuards,
+} from "@nestjs/common";
+import { FileFieldsInterceptor } from "@nestjs/platform-express";
 import {
   ApiBadRequestResponse,
   ApiBody,
@@ -11,57 +23,84 @@ import {
   ApiTags,
 } from "@nestjs/swagger";
 
+import { UseRoles } from "@/auth/decorators/role.decorator";
+import { JwtAuthGuard, RolesGuard } from "@/auth/guards";
+import { Role } from "@/auth/role.enum";
 import { BookService } from "./book.service";
-import { CreateBookDto } from "./dto/create-book.dto";
-import { UpdateBookDto } from "./dto/update-book.dto";
+import { CreateBookDto, UpdateBookDto, UploadBookDto } from "./dto/index";
 
 @ApiTags("books")
 @Controller({ version: "1", path: "books" })
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
+  // API Docs
   @ApiBody({ type: CreateBookDto })
   @ApiCreatedResponse({ description: "The record has been successfully created." })
   @ApiBadRequestResponse({ description: "The record has failed validation." })
   @ApiConflictResponse({ description: "The record has an internal conflict." })
   @ApiForbiddenResponse({ description: "Forbidden!" })
-  @Post()
-  create(@Body() createBookDto: CreateBookDto) {
-    return this.bookService.create(createBookDto);
+  // Interceptors
+  @UseRoles(Role.PUBLISHER, Role.ADMIN, Role.SUPER_ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: "cover", maxCount: 1 },
+      { name: "file", maxCount: 1 },
+    ])
+  )
+  // Method
+  @Post("create")
+  create(@UploadedFiles() files: UploadBookDto, @Body() createBookDto: CreateBookDto) {
+    return this.bookService.create(files, createBookDto);
   }
 
+  // API Docs
   @ApiOkResponse({ description: "The record has been successfully returned." })
   @ApiNotFoundResponse({ description: "The record was not found." })
   @ApiForbiddenResponse({ description: "Forbidden!" })
+  // Method
   @Get()
   index() {
     return this.bookService.findAll();
   }
 
+  // API Docs
   @ApiOkResponse({ description: "The record has been successfully returned." })
   @ApiNotFoundResponse({ description: "The record was not found." })
   @ApiForbiddenResponse({ description: "Forbidden!" })
   @ApiParam({ name: "id", type: "String", description: "Book id" })
+  // Method
   @Get(":id")
   find(@Param("id") id: string) {
-    return this.bookService.findOne(+id);
+    return this.bookService.findOne(id);
   }
 
+  // API Docs
   @ApiOkResponse({ description: "The record has been successfully returned." })
   @ApiNotFoundResponse({ description: "The record was not found." })
   @ApiForbiddenResponse({ description: "Forbidden!" })
   @ApiParam({ name: "id", type: "String", description: "Book id" })
+  // Interceptors
+  @UseRoles(Role.ADMIN, Role.SUPER_ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  // Method
   @Patch(":id")
   update(@Param("id") id: string, @Body() updateBookDto: UpdateBookDto) {
-    return this.bookService.update(+id, updateBookDto);
+    return this.bookService.update(id, updateBookDto);
   }
 
+  // API Docs
   @ApiOkResponse({ description: "The record has been successfully returned." })
   @ApiNotFoundResponse({ description: "The record was not found." })
   @ApiForbiddenResponse({ description: "Forbidden!" })
   @ApiParam({ name: "id", type: "String", description: "Book id" })
+  // Interceptors
+  @UseRoles(Role.ADMIN, Role.SUPER_ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  // Method
   @Delete(":id")
   remove(@Param("id") id: string) {
-    return this.bookService.remove(+id);
+    return this.bookService.remove(id);
   }
 }
