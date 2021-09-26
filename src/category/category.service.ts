83b10c73508a4a80cc08c2bec26531624e3c664a
Kerosz <andrei_chirila@outlook.com>
2021-09-26 18:35:02 +0300

    feat: add category service

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,28 +1,85 @@
-import { Injectable, Logger } from "@nestjs/common";
+import { ConflictException, Injectable, Logger, NotFoundException } from "@nestjs/common";
 import { CreateCategoryDto } from "./dto/create-category.dto";
 import { UpdateCategoryDto } from "./dto/update-category.dto";
+import { CategoryRepository } from "./models/category.repository";
 
 @Injectable()
 export class CategoryService {
-  create(createCategoryDto: CreateCategoryDto) {
-    Logger.log(createCategoryDto);
-    return "This action adds a new category";
+  constructor(private readonly categoryRepository: CategoryRepository) {}
+
+  public async create(c: CreateCategoryDto) {
+    const categoryRecord = await this.categoryRepository.findOne({ name: c.name });
+
+    if (categoryRecord) {
+      throw new ConflictException("Category already exists!");
+    }
+
+    const newCategoryRecord = await this.categoryRepository.create({
+      ...c,
+      parent: c.parent || null,
+    });
+    Logger.log(`Create new category: ${newCategoryRecord._id}`, CategoryService.name);
+
+    const { __v, ...newCategoryRecordWithoutVersion } = newCategoryRecord.toObject();
+
+    return newCategoryRecordWithoutVersion;
   }
 
-  findAll() {
-    return `This action returns all category`;
+  public async findAll() {
+    const categoryRecords = await this.categoryRepository.find();
+
+    if (!categoryRecords || categoryRecords.length < 1) {
+      throw new NotFoundException("No category records found!");
+    }
+    Logger.log(`Retrieved all categories`, CategoryService.name);
+
+    return categoryRecords;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
+  public async findOne(id: string) {
+    const categoryRecord = await this.categoryRepository.findById(id);
+
+    if (!categoryRecord) {
+      throw new NotFoundException("No category record found!");
+    }
+    Logger.log(`Retrieved category: ${categoryRecord._id}`, CategoryService.name);
+
+    const { __v, ...newCategoryRecordWithoutVersion } = categoryRecord.toObject();
+
+    return newCategoryRecordWithoutVersion;
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    Logger.log(updateCategoryDto);
-    return `This action updates a #${id} category`;
+  public async update(id: string, updateCategoryDto: UpdateCategoryDto) {
+    const categoryRecord = await this.categoryRepository.findById(id);
+
+    if (!categoryRecord) {
+      throw new NotFoundException("No category record found!");
+    }
+
+    const updatedCategoryRecord = await this.categoryRepository.findByIdAndUpdate(
+      id,
+      updateCategoryDto
+    );
+
+    Logger.log(
+      `Update category: ${categoryRecord._id}\n\n${updateCategoryDto}`,
+      CategoryService.name
+    );
+
+    return updatedCategoryRecord;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} category`;
+  public async remove(id: string) {
+    const categoryRecord = await this.categoryRepository.findById(id);
+
+    if (!categoryRecord) {
+      throw new NotFoundException("No category record found!");
+    }
+
+    const deletedCategoryRecord = await this.categoryRepository.deleteOne({ _id: id });
+
+    Logger.log(`Delete category: ${categoryRecord._id}`, CategoryService.name);
+
+    return deletedCategoryRecord;
   }
 }
