a9f9e8b9c33220743e9d4e049bcc1e8b7fc460af
Kerosz <andrei_chirila@outlook.com>
2021-09-27 03:22:30 +0300

    chore: register author schema and repository

--- src/author/author.module.ts
+++ src/author/author.module.ts
@@ -1,9 +1,16 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+
 import { AuthorService } from "./author.service";
 import { AuthorController } from "./author.controller";
+import { AuthorEntity, AuthorEntitySchema } from "./models/author.entity";
+import { AuthorRepository } from "./models/author.repository";
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: AuthorEntity.name, schema: AuthorEntitySchema }]),
+  ],
   controllers: [AuthorController],
-  providers: [AuthorService],
+  providers: [AuthorService, AuthorRepository],
 })
 export class AuthorModule {}
