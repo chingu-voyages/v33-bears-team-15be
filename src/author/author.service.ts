29089d1985964c58ddfd772b1e74939805561206
Kerosz <andrei_chirila@outlook.com>
2021-09-28 01:26:08 +0300

    feat: add author service

--- src/author/author.service.ts
+++ src/author/author.service.ts
@@ -1,28 +1,109 @@
-import { Injectable, Logger } from "@nestjs/common";
+import { ConflictException, Injectable, Logger, NotFoundException } from "@nestjs/common";
 
+import { AuthorRepository } from "./models/author.repository";
 import { CreateAuthorDto, UpdateAuthorDto } from "./dto/index";
+import { formatStringForPath, ProcessFile } from "@/utils";
 
 @Injectable()
 export class AuthorService {
-  create(createAuthorDto: CreateAuthorDto) {
-    Logger.log(createAuthorDto, AuthorService.name);
-    return "This action adds a new author";
+  constructor(private readonly authorRepository: AuthorRepository) {}
+
+  private async processAvatarUpload(
+    file: Express.Multer.File,
+    authorName: string
+  ): Promise<string | null> {
+    if (file) {
+      const formattedAuthorName = formatStringForPath(authorName);
+
+      const pAvatar: ProcessFile = new ProcessFile({
+        fileBuffer: file.buffer,
+        fileType: "avatar",
+        subDir: formattedAuthorName,
+      });
+      const avatarPath: string = await pAvatar.resizeAndSave({ width: 100, height: 100 });
+      Logger.log("Author avatar has been processed and saved!", AuthorService.name);
+
+      return avatarPath;
+    }
+
+    Logger.log("No author avatar file has been uploaded!", AuthorService.name);
+    return null;
   }
 
-  findAll() {
-    return `This action returns all author`;
+  public async create(file: Express.Multer.File, a: CreateAuthorDto) {
+    const authorRecord = await this.authorRepository.findOne({ name: a.name });
+
+    if (authorRecord) {
+      throw new ConflictException("Author already exists!");
+    }
+
+    const avatarPath: string | null = await this.processAvatarUpload(file, a.name);
+
+    const newAuthorRecord = await this.authorRepository.create({
+      ...a,
+      avatar: avatarPath,
+      books: JSON.parse(a.books as unknown as string),
+    });
+    Logger.log(`Create new author: ${newAuthorRecord._id}`, AuthorService.name);
+
+    const { __v, ...newAuthorRecordWithoutVersion } = newAuthorRecord.toObject();
+
+    return newAuthorRecordWithoutVersion;
   }
 
-  findOne(id: string) {
-    return `This action returns a #${id} author`;
+  public async findAll() {
+    const authorRecords = await this.authorRepository.find();
+
+    if (!authorRecords || authorRecords.length < 1) {
+      throw new NotFoundException("No author records found!");
+    }
+    Logger.log(`Retrieved all authors`, AuthorService.name);
+
+    return authorRecords;
   }
 
-  update(id: string, updateAuthorDto: UpdateAuthorDto) {
-    Logger.log(updateAuthorDto, AuthorService.name);
-    return `This action updates a #${id} author`;
+  public async findOne(id: string) {
+    const authorRecord = await this.authorRepository.findById(id);
+
+    if (!authorRecord) {
+      throw new NotFoundException("No author record found!");
+    }
+    Logger.log(`Retrieved author: ${authorRecord._id}`, AuthorService.name);
+
+    return authorRecord;
   }
 
-  remove(id: string) {
-    return `This action removes a #${id} author`;
+  public async update(id: string, updateAuthorDto: UpdateAuthorDto) {
+    const authorRecord = await this.authorRepository.findById(id);
+
+    if (!authorRecord) {
+      throw new NotFoundException("No author record found!");
+    }
+
+    const updatedAuthorRecord = await this.authorRepository.findByIdAndUpdate(
+      id,
+      updateAuthorDto
+    );
+
+    Logger.log(
+      `Update author: ${authorRecord._id}\n\n${updateAuthorDto}`,
+      AuthorService.name
+    );
+
+    return updatedAuthorRecord;
+  }
+
+  public async remove(id: string) {
+    const authorRecord = await this.authorRepository.findById(id);
+
+    if (!authorRecord) {
+      throw new NotFoundException("No author record found!");
+    }
+
+    const deletedAuthorRecord = await this.authorRepository.deleteOne({ _id: id });
+
+    Logger.log(`Delete author: ${authorRecord._id}`, AuthorService.name);
+
+    return deletedAuthorRecord;
   }
 }
