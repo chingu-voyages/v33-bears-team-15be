b934efb4a383940e569d9a56d0cb7908f0cbdece
R2D2 <66644638+R2rit0@users.noreply.github.com>
2021-10-08 13:38:18 -0400

    Added new suggestions

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,4 +1,11 @@
-import { ApiBody, ApiTags } from "@nestjs/swagger";
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+} from "@nestjs/swagger";
 import {
   Controller,
   Get,
@@ -11,7 +18,7 @@ import {
 } from "@nestjs/common";
 import { UseRoles } from "@/auth/decorators/role.decorator";
 import { Role } from "@/auth/role.enum";
-import { JwtAuthGuard } from "@/auth/guards";
+import { JwtAuthGuard, RolesGuard } from "@/auth/guards";
 
 import { ReviewService } from "./review.service";
 import { CreateReviewDto } from "./dto/create-review.dto";
@@ -20,12 +27,16 @@ import { UpdateReviewDto } from "./dto/update-review.dto";
 const CONTROLLER_NAME = "reviews";
 
 @ApiTags(CONTROLLER_NAME)
-@UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller({ version: "1", path: CONTROLLER_NAME })
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
 
   @ApiBody({ type: CreateReviewDto })
+  @ApiOkResponse({ description: "The record has been successfully created." })
+  @ApiNotFoundResponse({ description: "The record was not found." })
+  @ApiBadRequestResponse({ description: "The record has failed validation." })
+  @ApiForbiddenResponse({ description: "Forbidden!" })
   @Post(":userId/create/:bookId")
   create(
     @Param("userId") userId: string,
@@ -35,22 +46,38 @@ export class ReviewController {
     return this.reviewService.create(userId, bookId, createReviewDto);
   }
 
+  @ApiOkResponse({ description: "The record has been successfully returned." })
+  @ApiNotFoundResponse({ description: "The record was not found." })
+  @ApiBadRequestResponse({ description: "The record has failed validation." })
+  @ApiForbiddenResponse({ description: "Forbidden!" })
   @Get()
   @UseRoles(Role.ADMIN, Role.SUPER_ADMIN)
   findAll() {
     return this.reviewService.findAll();
   }
 
+  @ApiOkResponse({ description: "The record has been successfully returned." })
+  @ApiNotFoundResponse({ description: "The record was not found." })
+  @ApiBadRequestResponse({ description: "The record has failed validation." })
+  @ApiForbiddenResponse({ description: "Forbidden!" })
   @Get(":reviewId")
   findOne(@Param("reviewId") id: string) {
     return this.reviewService.findOne(id);
   }
 
+  @ApiOkResponse({ description: "The record has been successfully returned." })
+  @ApiNotFoundResponse({ description: "The record was not found." })
+  @ApiBadRequestResponse({ description: "The record has failed validation." })
+  @ApiForbiddenResponse({ description: "Forbidden!" })
   @Get("/book/:bookId")
   findAllBookReviews(@Param("bookId") bookId: string) {
     return this.reviewService.findAllBookReviews(bookId);
   }
 
+  @ApiOkResponse({ description: "The record has been successfully updated." })
+  @ApiNotFoundResponse({ description: "The record was not found." })
+  @ApiBadRequestResponse({ description: "The record has failed validation." })
+  @ApiForbiddenResponse({ description: "Forbidden!" })
   @Patch(":userId/update-review/:reviewId")
   updateReview(
     @Param("userId") userId: string,
@@ -60,11 +87,19 @@ export class ReviewController {
     return this.reviewService.updateReview(userId, reviewId, updateReviewDto);
   }
 
+  @ApiOkResponse({ description: "The record has been successfully updated." })
+  @ApiNotFoundResponse({ description: "The record was not found." })
+  @ApiBadRequestResponse({ description: "The record has failed validation." })
+  @ApiForbiddenResponse({ description: "Forbidden!" })
   @Patch(":userId/update-helpful/:reviewId")
   updateHelpful(@Param("userId") userId: string, @Param("reviewId") reviewId: string) {
     return this.reviewService.updateHelpful(userId, reviewId);
   }
 
+  @ApiOkResponse({ description: "The record has been successfully removed." })
+  @ApiNotFoundResponse({ description: "The record was not found." })
+  @ApiBadRequestResponse({ description: "The record has failed validation." })
+  @ApiForbiddenResponse({ description: "Forbidden!" })
   @Delete(":userId/delete/:reviewId/:bookId")
   remove(
     @Param("userId") userId: string,
